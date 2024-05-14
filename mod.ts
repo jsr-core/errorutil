8dcdd302679c7a156798e61d22b29870f40c2800
Alisue <lambdalisue@gmail.com>
2024-05-14 11:21:59 +0900

    :memo: Deprecate packages on deno.land or npm

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 export * from "./error_object.ts";
 export * from "./raise.ts";
 export * from "./try_or.ts";
+export * from "./try_or_else.ts";
 export * from "./unimplemented.ts";
 export * from "./unreachable.ts";
--- try_or.ts
+++ try_or.ts
@@ -1,9 +1,10 @@
+import { tryOrElse } from "./try_or_else.ts";
 /**
  * Try to execute a function and return the result or a default value.
  *
  * ```ts
- * import { tryOr } from "https://deno.land/x/errorutil@$MODULE_VERSION/try_or.ts";
- * import { raise } from "https://deno.land/x/errorutil@$MODULE_VERSION/raise.ts";
+ * import { tryOr } from "@lambdalisue/errorutil/try-or";
+ * import { raise } from "@lambdalisue/errorutil/raise";
  *
  * // Sync
  * console.log(tryOr(() => 1, 2)); // 1
@@ -24,40 +25,4 @@ export function tryOr<T>(
   orValue: T,
 ): T | Promise<T> {
   return tryOrElse(fn, () => orValue);
-}
-
-/**
- * Try to execute a function and return the result or execute another function.
- *
- * ```ts
- * import { tryOrElse } from "https://deno.land/x/errorutil@$MODULE_VERSION/try_or.ts";
- * import { raise } from "https://deno.land/x/errorutil@$MODULE_VERSION/raise.ts";
- *
- * // Sync
- * console.log(tryOrElse(() => 1, () => 2)); // 1
- * console.log(tryOrElse(() => raise("err"), () => 2)); // 2
- *
- * // Async
- * console.log(await tryOrElse(() => Promise.resolve(1), () => 2)); // 1
- * console.log(await tryOrElse(() => Promise.reject("err"), () => 2)); // 2
- * ```
- */
-export function tryOrElse<T>(fn: () => T, elseFn: (err: unknown) => T): T;
-export function tryOrElse<T>(
-  fn: () => Promise<T>,
-  elseFn: (err: unknown) => T | Promise<T>,
-): Promise<T>;
-export function tryOrElse<T>(
-  fn: () => T | Promise<T>,
-  elseFn: (err: unknown) => T | Promise<T>,
-): T | Promise<T> {
-  try {
-    const ret = fn();
-    if (ret instanceof Promise) {
-      return ret.catch((err) => elseFn(err));
-    }
-    return ret;
-  } catch (err) {
-    return elseFn(err);
-  }
 }
